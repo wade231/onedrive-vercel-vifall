16ee2cc0417b98aabaf8a2c0e7acf0d1a9902807
wade231 <122469077+wade231@users.noreply.github.com>
2023-09-26 11:08:17 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,8 +9,8 @@ const createFooterMarkup = () => {
 const Footer = () => {
   return (
 <div
-  className="w-full border-t border-gray-900/10 p-4 text-center text-xs font-medium text-gray-400 dark:border-gray-500/30"
-  style={{ lineHeight: '10' }}
+  className="w-full border-t border-gray-900/10 p-32 text-center text-xs font-medium text-gray-400 dark:border-gray-500/30"
+  style={{ lineHeight: '8' }}
   dangerouslySetInnerHTML={createFooterMarkup()}
 ></div>
 
